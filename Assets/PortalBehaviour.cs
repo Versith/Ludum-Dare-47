3ce74ddb40096d4bd0b74f7c0e2b02e5002746ef
Benjamin Ostendorf <5ostendo@informatik.uni-hamburg.de>
2020-10-03 19:00:43 +0200

    portal teleport is working

--- Assets/PortalBehaviour.cs
+++ Assets/PortalBehaviour.cs
@@ -9,7 +9,19 @@ public class PortalBehaviour : MonoBehaviour
     public Transform playerCamera;
     public Transform portal;
     public Transform otherPortal;
-    public Transform camera;
+    public Transform portalCamera;
+
+    private bool playerIsOverlapping = false;
+    private float lastTeleportTime = 0;
+    private Transform player;
+
+    public bool isActive = true;
+
+    private void Start()
+    {
+        player = playerCamera.transform.parent;
+    }
+
 
     private void Update()
     {
@@ -21,18 +33,55 @@ public class PortalBehaviour : MonoBehaviour
         Vector3 oppositeNormalOfOtherPortal = otherPortal.transform.up;
         //We reflect at the negative normal of the portal surface
         Vector3 reflectedPosition = -Vector3.Reflect(localPlayerPosition, oppositeNormalOfOtherPortal);
-        camera.transform.position = portal.TransformPoint(reflectedPosition);
+        portalCamera.transform.position = portal.TransformPoint(reflectedPosition);
 
         //We take the forward vector of the players camera in the other portals local space and apply the the negative forward direction
         //to this portals camera. Only the y component of the forward vector isn't changed to prevent vertical view inversion.
         Vector3 playerCamForwardLocal = otherPortal.InverseTransformVector(playerCamera.forward);
         Vector3 newForward = -portal.transform.TransformVector(playerCamForwardLocal);
-        camera.transform.forward = new Vector3(newForward.x, -newForward.y, newForward.z);
+        portalCamera.transform.forward = new Vector3(newForward.x, -newForward.y, newForward.z);
+
+        if(lastTeleportTime <= Time.time - 0.5f)
+        {
+            otherPortal.gameObject.GetComponentInChildren<PortalBehaviour>().isActive = true;
+        }
+
+        if (isActive && playerIsOverlapping)
+        {
+            otherPortal.gameObject.GetComponentInChildren<PortalBehaviour>().isActive = false;
+            lastTeleportTime = Time.time;
+            CharacterController controller = playerCamera.GetComponentInParent<CharacterController>();
+            controller.enabled = false;
+            localPlayerPosition = portal.InverseTransformPoint(playerCamera.position);
+            oppositeNormalOfOtherPortal = portal.transform.up;
+            reflectedPosition = -Vector3.Reflect(localPlayerPosition, oppositeNormalOfOtherPortal);
+            player.position = otherPortal.TransformPoint(reflectedPosition);
+
+            Vector3 playerForwardLocal = portal.InverseTransformVector(player.forward);
+            newForward = -otherPortal.transform.TransformVector(playerForwardLocal);
+            player.transform.forward = new Vector3(newForward.x, -newForward.y, newForward.z);
+
+            controller.enabled = true;
+            playerIsOverlapping = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger enter");
+        Debug.Log("Trigegr");
+
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
     }
 
 }
