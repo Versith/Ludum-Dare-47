e1288ddc0b6a83880f1b0f0fbc7198a0c98b177b
Benjamin Ostendorf <5ostendo@informatik.uni-hamburg.de>
2020-10-03 16:15:10 +0200

    camera movement for portals works correctly. Stencil Buffer for portal view implemented. Bug in portal camera rotation (camera does not rotate with respect to portal surface rotation)

---
+++ Assets/PortalBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class PortalBehaviour : MonoBehaviour
+{
+    public Transform playerCamera;
+    public Transform portalCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+    private void Update()
+    {
+        //Get the position of the player in otherPortal space
+        Vector3 localPlayerPosition = otherPortal.InverseTransformPoint(playerCamera.position);
+        Debug.Log("localPlayerPosition = " + localPlayerPosition);
+        //Vector3 oppositeNormalOfOtherPortal = otherPortal.GetComponent<Plane>().normal * -1;
+        
+        //evt. hier auch forward und nicht invertieren des reflect vectors.
+        Vector3 oppositeNormalOfOtherPortal = otherPortal.transform.up;
+        Vector3 reflectedPosition = -Vector3.Reflect(localPlayerPosition, oppositeNormalOfOtherPortal);
+        transform.position = portal.TransformPoint(reflectedPosition);
+        
+        //Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        //transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, -Vector3.up);
+        Vector3 newCameraDirection = portalRotationDifference * -playerCamera.forward;
+        newCameraDirection = new Vector3(newCameraDirection.x, -newCameraDirection.y, newCameraDirection.z);
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    }
+
+}
---
+++ Assets/PortalTextrureSetup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextrureSetup : MonoBehaviour
+{
+    public Camera cameraPortal1;
+    public Camera cameraPortal2;
+
+    public Material camera1Mat;
+    public Material camera2Mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(cameraPortal1.targetTexture != null)
+        {
+            cameraPortal1.targetTexture.Release();
+        }
+        if (cameraPortal2.targetTexture != null)
+        {
+            cameraPortal2.targetTexture.Release();
+        }
+        cameraPortal1.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraPortal2.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+
+        camera1Mat.mainTexture = cameraPortal1.targetTexture;
+        camera2Mat.mainTexture = cameraPortal2.targetTexture;
+    }
+
+}
