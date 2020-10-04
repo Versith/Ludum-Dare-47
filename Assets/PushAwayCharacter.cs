3246db28b6727b1a92cbffd32af98c7208191f63
5kramer <5kramer@informatik.uni-hamburg.de>
2020-10-04 19:21:18 +0200

    added rotating bars

---
+++ Assets/PushAwayCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushAwayCharacter : MonoBehaviour
+{
+    [SerializeField] private float _pushStrength = 50f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Player") && collision.collider.GetComponent<PlayerMovement>() != null)
+        {
+            Vector3 normal = collision.GetContact(0).normal;
+            normal = new Vector3(-normal.x, 0, -normal.z);
+            collision.collider.GetComponent<PlayerMovement>().GetPushed(normal, _pushStrength);
+        }
+    }
+}
