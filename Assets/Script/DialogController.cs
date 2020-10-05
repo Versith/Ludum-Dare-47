fcefcbed53d49055302ba918869979ddb786ba09
Benjamin Ostendorf <5ostendo@informatik.uni-hamburg.de>
2020-10-05 22:08:17 +0200

    change exit portal position in startroom

--- Assets/Script/DialogController.cs
+++ Assets/Script/DialogController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using UnityEditor.SceneManagement;
+//using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
 
