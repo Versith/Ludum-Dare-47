085ca50726cf36c6cd0ff74d8cc4c782e2e3c7de
5kramer <5kramer@informatik.uni-hamburg.de>
2020-10-05 15:11:33 +0200

    added front menu

---
+++ Assets/BGMPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioClip _clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource source = transform.GetComponent<AudioSource>();
+        source.clip = _clip;
+        source.Play();
+    }
+}
